6992518d524753bd29c47dcdab8ee0a254b9b8f1
Omohemma <emmaomoh@gmail.com>
2021-08-27 17:14:32 +0100

    E-commerce Homepage + SASS setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Homepage from './homepage.component';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!!</h1>
+      <Homepage />
     </div>
   );
 }
