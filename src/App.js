f6021a7925b7d34c2a446884d8fe55f3e0d78cd4
Omohemma <emmaomoh@gmail.com>
2021-09-06 17:21:00 +0100

    Routing Between Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage =  (props) => (
+  <div>
+    <h1>This is the Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage}></Route>
+        <Route exact path='/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
