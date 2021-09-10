59ed0bf988a295200d17b2276fc9c938019aae0f
Omohemma <emmaomoh@gmail.com>
2021-09-10 17:27:56 +0100

    Signin Component & Auth Page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import {Route, Switch} from 'react-router-dom';
-
+import AuthPage from './pages/auth/auth.component';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Switch>
-        <Route exact path='/' component={Homepage}></Route>
-        <Route exact path='/shop' component={ShopPage}></Route>
+        <Route exact path="/" component={Homepage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+        <Route exact path="/signin" component={AuthPage}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,11 @@
+import SignIn from '../../components/sign-in/sign-in.component';
+import './auth.styles.scss';
+
+const AuthPage = () => (
+  <div>
+    <h1>Sign In</h1>
+    <SignIn />
+  </div>
+);
+
+export default AuthPage;
