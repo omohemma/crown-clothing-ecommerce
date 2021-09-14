0e5bde640f08bdbccec1eadfb07b4e47185600a9
Omohemma <emmaomoh@gmail.com>
2021-09-14 20:03:36 +0100

    Convert App to Class Component To store loggedIn user

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
+import {Component} from "react";
+
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import AuthPage from './pages/auth/auth.component';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage}></Route>
-        <Route exact path="/shop" component={ShopPage}></Route>
-        <Route exact path="/sign-in" component={AuthPage}></Route>
-      </Switch>
-    </div>
-  );
+class App extends Component() {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser : null
+    }
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage}></Route>
+          <Route exact path="/shop" component={ShopPage}></Route>
+          <Route exact path="/sign-in" component={AuthPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
