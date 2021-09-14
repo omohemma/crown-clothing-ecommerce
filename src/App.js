a29277382a679c47ed4875103d9206eed63fb3ba
Omohemma <emmaomoh@gmail.com>
2021-09-14 20:13:18 +0100

    Open Subscription on Auth

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import AuthPage from './pages/auth/auth.component';
+import {auth} from "./firebase/firebase.utils";
 
-class App extends Component() {
+
+class App extends Component {
   constructor(){
     super();
 
     this.state = {
       currentUser : null
     }
+  }
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user => {
+      this.setState({currentUser : user});
+      console.log(user)
+    })
+
 
   }
 
