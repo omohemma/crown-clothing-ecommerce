835b24a9a8019505f722e45e34629c311fc13140
Omohemma <emmaomoh@gmail.com>
2021-09-23 19:06:38 +0100

    Setting Up Redux

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,26 @@
+// Initial State
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+/* Initialize state params with user reducer initial state
+ ** action is an object with "type" and "payload" key
+ */
+const userReducer = (state = INITIAL_STATE, action) => {
+  /* Use Switch Case Instead of If...else statement to know with state to
+   ** Return New State - other objects in INITIAL_STATE and payload in "action object"
+   ** Or by default return state
+   */
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
