4e202bf9fc69470490a1a87a2efce2fc52d97eb2
Omohemma <emmaomoh@gmail.com>
2021-09-24 08:34:42 +0100

    Redux Store

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.render(
-  //Provider "Component" enables us to Access State in all 
+  //Provider "Component" enables us to Access State in all
   // components - root level implementation
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
