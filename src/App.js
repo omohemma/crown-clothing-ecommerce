0c1b23f0411cc22c72b0620a4e35ca4c014bc940
Omohemma <emmaomoh@gmail.com>
2021-09-24 11:11:39 +0100

    Redirect User - Auth Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -46,16 +46,26 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Homepage}></Route>
           <Route exact path="/shop" component={ShopPage}></Route>
-          <Route exact path="/sign-in" component={AuthPage}></Route>
+          <Route
+            exact
+            path="/sign-in"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <AuthPage />
+            }
+          ></Route>
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-// arg1 is null because App Component doesn't use the state
-export default connect(null, mapDispatchToProps)(App);
+// arg1 will null because App Component doesn't use the state
+export default connect(mapStateToProps, mapDispatchToProps)(App);
