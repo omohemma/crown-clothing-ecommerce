a750b6972863d7b87445193bacbd6c358b2ba3db
Omohemma <emmaomoh@gmail.com>
2021-09-28 13:12:09 +0100

    Toggle Cart Hidden Redux Setup

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
   hidden: true,
 };
 
-const CartReducer = (state = INITIAL_STATE, action) => {
+const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
@@ -16,4 +16,4 @@ const CartReducer = (state = INITIAL_STATE, action) => {
   }
 };
 
-export default CartReducer;
+export default cartReducer;
