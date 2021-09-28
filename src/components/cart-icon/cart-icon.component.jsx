bb96ab6f481e27f721fe6ce9fd58b02013f069e8
Omohemma <emmaomoh@gmail.com>
2021-09-28 13:21:38 +0100

    Implement Cart Action On Cart Icon Component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,19 @@
 import { ReactComponent as ShoppingBag } from '../../assets/shopping-bag.svg';
 
+import { connect } from 'react-redux';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
-  <div className="cart-icon">
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingBag className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
