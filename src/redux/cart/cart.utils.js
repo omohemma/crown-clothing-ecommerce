c3488852d93e5837fe57bcb1ceb43d7a65b537c9
Omohemma <emmaomoh@gmail.com>
2021-09-29 23:31:01 +0100

    cart utils to group items, update shop data, update cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const groupCartItems = (cartItems, cartItemToAdd) => {
+  // Find If Item with a SImilar Unique Identifier Exist
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // Return Previous State and Initialize Quantity of New Item Add to Cart
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
