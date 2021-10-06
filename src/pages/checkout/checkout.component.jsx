dead21ce43ec29e9d6e92cd563a7541a74e9408a
Omohemma <emmaomoh@gmail.com>
2021-10-06 07:42:51 +0100

    checkout page design

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,24 @@
 import './checkout.styles.scss';
 
 const Checkout = () => (
-  <div>
-    <h1>Checkout Page</h1>
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
+    </div>
   </div>
 );
 
