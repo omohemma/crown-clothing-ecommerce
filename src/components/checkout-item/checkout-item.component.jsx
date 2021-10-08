806c606bb4cb4986bf223d159ad05ff95f150203
Omohemma <emmaomoh@gmail.com>
2021-10-08 07:30:37 +0100

    increment quantity of an item on checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,11 @@
 import { connect } from 'react-redux';
-import { removeItemsFromCart } from '../../redux/cart/cart.actions';
+import {
+  addItemToCart,
+  removeItemsFromCart,
+} from '../../redux/cart/cart.actions';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, removeItem }) => {
+const CheckoutItem = ({ cartItem, removeItem, addItem }) => {
   const { name, price, quantity, imageUrl } = cartItem;
   return (
     <div className="checkout-item">
@@ -14,7 +17,9 @@ const CheckoutItem = ({ cartItem, removeItem }) => {
       <span className="quantity">
         <div className="arrow">&#10094;</div>
         <span className="value">{quantity}</span>
-        <div className="arrow">&#10095;</div>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
       </span>
 
       <span className="price">{price}</span>
@@ -27,6 +32,7 @@ const CheckoutItem = ({ cartItem, removeItem }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   removeItem: (item) => dispatch(removeItemsFromCart(item)),
+  addItem: (item) => dispatch(addItemToCart(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
